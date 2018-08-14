c17190e6331eb01cd99c700628a591f683336482
iank <iank@localhost.localdomain>
2018-08-14 14:53:27 +0700

    Edit: Cleaning Code

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,6 @@ from .configs import Config
 from sanic_cors import CORS
 from sanic.response import json
 
-# app = Sanic(__name__)
-
-# @app.route("/")
-# async def index():
-#     return json("Welcome")
-
-
-# if __name__ == '__main__':
-#     app.run()
 
 def create_app():
     app = Sanic(__name__)
